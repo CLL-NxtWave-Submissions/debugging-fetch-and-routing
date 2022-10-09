a021a5c34c48b43c4f19c89eedb18ff040f9a43b
Surya T N <suryatej.nim@yahoo.com>
2022-10-09 17:12:46 +0000

    Fixed the typo of the path in child Route component instance of parent App component, to map About component to '/about' path to match the path in child Link component in parent Header component

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => (
         <Header />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/abou" component={About} />
+          <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
           <Route path="/blogs/id" component={BlogItemDetails} />
           <Route component={NotFound} />
